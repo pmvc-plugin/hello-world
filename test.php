8f832d055750937d80f913674edca31803aae2a3
Hill <hill@kimo.com>
2015-07-04 23:25:01 +0400

    add test plugin unit test

--- test.php
+++ test.php
@@ -3,6 +3,16 @@ PMVC\Load::plug();
 PMVC\setPlugInFolder('../');
 class HelloTest extends PHPUnit_Framework_TestCase
 {
+    function testPlugin()
+    {
+        ob_start();
+        $plug = 'hello_world';
+        print_r(PMVC\plug($plug));
+        $output = ob_get_contents();
+        ob_end_clean();
+        $this->assertContains($plug,$output);
+    }
+
     function testHello()
     {
         $willSay = 'hello, World!';
